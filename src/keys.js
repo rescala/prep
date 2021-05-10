85855a62fcff30b7ddda9a9c7b23c12f72f744b3
luisrescala <luis.rescala@gmail.com>
2021-05-09 20:08:18 -0500

    Commit para nuevo servidor

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,15 @@
 module.exports = {
     database:{
-        host: 'localhost',
-        user: 'prepuser',
-        password: 'prep123',
-        database: 'prep_tar_pt'
+        host: '72.167.34.131',
+        user: 'luisresc_prepuser',
+        password: 'aS4-+qgm,SRc',
+        database: 'luisresc_prep_cuit_pt'
     }
 };
 
 // prepuser
 // prep123
+
+
+// luisresc_prepuser
+// aS4-+qgm,SRc
